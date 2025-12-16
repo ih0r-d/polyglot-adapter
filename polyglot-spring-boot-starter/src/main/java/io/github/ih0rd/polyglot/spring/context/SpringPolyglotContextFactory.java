db83b77cc5a8a47c4aed0bfef78fa80e790df8bf
Ihor Didyk <ihordidyk95@gmail.com>
2025-12-16 22:25:18 +0200

    feat: initial Spring Boot starter for polyglot-adapter

---
+++ polyglot-spring-boot-starter/src/main/java/io/github/ih0rd/polyglot/spring/context/SpringPolyglotContextFactory.java
@@ -0,0 +1,22 @@
+package io.github.ih0rd.polyglot.spring.context;
+
+import org.graalvm.polyglot.Context;
+import org.springframework.beans.factory.ObjectProvider;
+
+import io.github.ih0rd.adapter.context.PolyglotHelper;
+import io.github.ih0rd.adapter.context.SupportedLanguage;
+
+public final class SpringPolyglotContextFactory {
+
+  private final ObjectProvider<PolyglotContextCustomizer> customizers;
+
+  public SpringPolyglotContextFactory(ObjectProvider<PolyglotContextCustomizer> customizers) {
+    this.customizers = customizers;
+  }
+
+  public Context create(SupportedLanguage language) {
+    return PolyglotHelper.newContext(
+        language,
+        builder -> customizers.orderedStream().forEach(c -> c.customize(language, builder)));
+  }
+}
